5cde5a9119c516d3b68e5aeffb9111cf9b1ff96d
Gede Lumbung <gedesumawijaya@gmail.com>
2017-10-29 03:52:37 +0800

    :sparkles: toolbox cache & load image from service worker

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -108,7 +108,28 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/code\.getmdl\.io\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/randomuser\.me\/api\/portraits\/men/,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/randomuser\.me\/api\/portraits\/women/,
+        handler: 'cacheFirst'
+      }]
     })
   ]
 })
